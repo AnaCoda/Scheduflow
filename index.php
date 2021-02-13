4bb9b22ee6da9d7a8d6b3ce403ff9105144b6d1f
Jim Geng <jimgeng2004@gmail.com>
2021-02-13 13:41:23 -0700

    navbar bascially complete

--- index.php
+++ index.php
@@ -5,14 +5,23 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/style.css">
+	<script src="https://kit.fontawesome.com/38b15697bc.js" crossorigin="anonymous"></script>
 	<title>Document</title>
 </head>
 <body>
 	<nav class="navbar">
 		<ul class="navicons topicons">
-			<li></li>
-			<li></li>
-			<li></li>
+			<li><i class="fas fa-home wideboi"></i></li>
+			<li><i class="fas fa-chart-bar wideboi"></i></li>
+			<li><i class="fas fa-sticky-note"></i></li>
+		</ul>
+		<ul class="navicons boticons">
+			<li><i class="fas fa-cog settings-icon"></i></li>
+			<li>
+				<div class="user-icon">
+					<?php echo file_get_contents("img/profile-pic.svg");?>
+				</div>
+			</li>
 		</ul>
 	</nav>
 	<main class="content">

52a7d9f18f653140dcaf36c3d2eac5e96941ac0c
Ana Coda <anacod@zoho.com>
2021-02-13 13:57:44 -0700

    Added user ids to tables, started profile page, fixed dbconnection

--- SQL/scheduflow_db.sql
+++ SQL/scheduflow_db.sql
@@ -10,12 +10,14 @@ CREATE TABLE `users` (
 
 CREATE TABLE `topics` (
   `topic_id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
   `topicname` text NOT NULL,
   PRIMARY KEY (`topic_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `events` (
   `event_id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
   `eventname` text NOT NULL,
   `eventtime` DATE NOT NULL, /*Only if the eventtype is Day(0) or sUntil(2)*/
   `eventtype` int(1) NOT NULL, /*(0)Single day, deletes itself after completion. (1)Indefinite, requires user deletion. (2)Until, will delete after certain calendar day*/
--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,5 @@
 <?php
 $dbpass = file_get_contents('password.txt');
 
-$curLink = mysqli_connect("localhost", "root", $dpbass, "scheduflow_db") or die(printf(mysqli_error($curLink))); // Connect to database server(localhost) with username and password.
+$curLink = mysqli_connect("localhost", "root", $dbpass, "scheduflow_db") or die(printf(mysqli_error($curLink))); // Connect to database server(localhost) with username and password.
 ?>

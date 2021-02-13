d7b0103799fddfbb1129be13cfc6d53062815669
Ana Coda <anacod@zoho.com>
2021-02-13 15:51:49 -0700

    bars finally count down wow

--- js/chart.js
+++ js/chart.js
@@ -1,19 +1,35 @@
 /** Code By Webdevtrick ( https://webdevtrick.com ) **/
+
 setTimeout(function start() {
 
-    $('.bar').each(function(i) {
-        var $bar = $(this);
-        $(this).append('<span class="count"></span>')
+    function progress(timeleft, timetotal, $element) {
+
+
+        var progressBarWidth = timeleft * 10;
+        console.log(progressBarWidth);
+        $element.animate({ width: progressBarWidth }, 1000);
+        $element.append('<span class="count"></span>')
+        if (timeleft > 0) {
+            setTimeout(function() {
+                progress(timeleft - 1, timetotal, $element);
+            }, 1000); //Every second, rerun this to make the progress bar smaller
+        }
+
+        /*$(this).append('<span class="count"></span>')
         setTimeout(function() {
             $bar.css('width', $bar.attr('data-percent'));
-        }, i * 100);
+        }, i * 1000);*/
+    }
+    // Do this for every bar (will change to only selected bar)
+    $('.bar').each(function(i) {
+        var $bar = $(this);
+        progress(parseFloat($bar.attr('data-percent')), 100, $bar);
     });
-
     $('.count').each(function() {
-        $(this).prop('Counter', 0).animate({
-            Counter: $(this).parent('.bar').attr('data-percent')
+        $(this).prop('Counter', /*count from the percent*/ parseFloat($(this).parent('.bar').attr('data-percent'))).animate({
+            Counter: 0 //count down to zero
         }, {
-            duration: 2000,
+            duration: 100000,
             easing: 'swing',
             step: function(now) {
                 $(this).text(Math.ceil(now) + '%');

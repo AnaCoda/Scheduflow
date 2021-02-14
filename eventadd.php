146ddb97dcdc2fa3c024a4813b86fdb290f634cf
anotherbeaver <45208549+anotherbeaver@users.noreply.github.com>
2021-02-13 18:54:40 -0700

    help ana oh great and venerable god of goodness

--- eventadd.php
+++ eventadd.php
@@ -14,8 +14,10 @@
               ?><h1>Select A Date</h1> <!--Jim-senpai pls add css and classes thx-->
               <form action="eventadd.php" method="post" autocomplete="off">
                   <input type="date" name="selecteddate">
-                 </form><?php
+                 <?php
             } else {
+              ?>
+              <form action="eventadd.php" method="post" autocomplete="off"> <?php
               $selecteddate = null;
             }
             if(isset($_POST['eventname'])){
@@ -24,35 +26,30 @@
             if(isset($_POST['selecteddate'])){
               $selecteddate = $_POST['selecteddate'];
             }
-            
-            if(isset($_POST['selectedtopic']) && isset($eventname) && $eventname != '' && isset($selecteddate) && $selecteddate != ''){
+            if(isset($_POST['selectedtopic']) && isset($eventname) && $eventname != ''/* && isset($selecteddate) && $selecteddate != ''*/){
               ?><h1>Event name <?php echo $eventname?></h1>
               <?php
               $eventname = $curLink->real_escape_string($_POST['eventname']);
               $selectedtopic = $curLink->real_escape_string($_POST['selectedtopic']);
-              $selecteddate = $_POST['']
               ?><h1>Parameters: eventname: <?php echo $eventname?> selectedtopic: <?php echo $selectedtopic?></h1>
               <?php
               $curLink->query("INSERT INTO events (user_id, eventtopic, eventname, eventtime, eventtype) VALUES(
                   '".$_SESSION['id']."',
                   '$selectedtopic',
                   '$eventname',
-                  '$selecteddate',
+                  null,
                   0)") or die(mysqli_error($curLink));
               header('Refresh: 0');
             }
               ?>
-              <form action="eventadd.php" method="post" autocomplete="off">
-                <h1>Step Two: Name your Task:</h1></div>
-                <input type="text" name="eventname">
-                </div><br>
+                <h1>Step Two: Name your Task:</h1>
+                <input type="text" name="eventname"><br>
                 <h1>Step Three: Choose a Topic</h1>
                 <?php 
               $temp = $curLink->query("SELECT * FROM `topics`");
               $topics = $temp->fetch_all(MYSQLI_ASSOC);
                 foreach ($topics as $t) { ?>
               <div>
-                
                   <input type="submit" name="selectedtopic" value="<?= $t['topicname']?>">
                 </div>
                 <?php }

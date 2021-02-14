6f92c98b7378fc51ba74e61062327e451534d1fd
Jim Geng <jimgeng2004@gmail.com>
2021-02-13 22:21:46 -0700

    fixed bad css

--- schedule.php
+++ schedule.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <!-- Code By Webdevtrick ( https://webdevtrick.com )-->
-<html lang="en" >
+<html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <title>Your Schedule</title>
@@ -8,42 +9,45 @@
   <link rel="stylesheet" href="css/chart.css">
 </head>
 <?php
-  session_start();
-  // Check that they're logged in, otherwise redirect
-  if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
-  {
-    require "dbconnection.php";
-    $id = $_SESSION['id'];
-    $eventquery = $curLink->query("SELECT * FROM `events` WHERE user_id='$id'") or die(printf(mysqli_error($curLink)));
-    $events = $eventquery->fetch_all(MYSQLI_ASSOC);
-    $topicquery = $curLink->query("SELECT * FROM `topics` WHERE user_id='$id'") or die(printf(mysqli_error($curLink)));
-    $topics = $topicquery->fetch_all(MYSQLI_ASSOC);
-  }
-  else
-    {
-        header("Location: login.php"); // Redirect to the login page
-    }
+session_start();
+// Check that they're logged in, otherwise redirect
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+  require "dbconnection.php";
+  $id = $_SESSION['id'];
+  $eventquery = $curLink->query("SELECT * FROM `events` WHERE user_id='$id'") or die(printf(mysqli_error($curLink)));
+  $events = $eventquery->fetch_all(MYSQLI_ASSOC);
+  $topicquery = $curLink->query("SELECT * FROM `topics` WHERE user_id='$id'") or die(printf(mysqli_error($curLink)));
+  $topics = $topicquery->fetch_all(MYSQLI_ASSOC);
+} else {
+  header("Location: login.php"); // Redirect to the login page
+}
 ?>
-<body>
-   <div class="wrap">
-<h1>Your Schedule</h1>
-<?php 
-          foreach ($topics as $t) { ?>
-          <!--<div class="bar" data-percent="20"> <span class="label"></span></div>-->
-          <div class="row">
-          <div id="container chart">
-        <div class="bar stacked" data-percent="40"><span class="label second"><?= $t['topicname'] ?></span></div><div class="bar stacked stackeder" data-percent="10"><span class="labelstack"></span></div>
-                  </div>
-          </div>
-          </div>
-<?php } ?>
-<div id="container chart">
-<div class="bar stacked" data-percent="40"><span class="label second">jQuery</span></div><div class="bar stacked stackeder" id='moving-bar' data-percent="10"><span class="labelstack"></span></div>
-          </div>
-  </div>
 
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script  src="js/chart.js"></script>
+<body>
+  <h1>Your Schedule</h1>
+  <div class="wrap">
+    <!--<div class="bar" data-percent="20"> <span class="label"></span></div>-->
+    <?php
+    foreach ($topics as $t) { ?>
+      
+      <div class="chart">
+        <span class="label"><?= $t['topicname'] ?></span>
+        <div class="bar" data-percent="40"></div>
+        <div class="bar" data-percent="10"><span class="labelstack"></span></div>
+      </div>
+    <?php } ?>
+    <div class="chart">
+      <span class="label">jQuery</span>
+      <div class="bar" data-percent="40"></div>
+      <div class="bar" id='moving-bar' data-percent="10"><span class="labelstack"></span></div>
+    </div>
+    </div>
+</div>
+
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="js/chart.js"></script>
 
 </body>
+
 </html>
+

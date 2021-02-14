05fb181008050ef66569ec09ef398ed8b08564f4
anotherbeaver <45208549+anotherbeaver@users.noreply.github.com>
2021-02-13 18:14:57 -0700

    Pls help ana my god im dying here

--- SQL/scheduflow_db.sql
+++ SQL/scheduflow_db.sql
@@ -20,7 +20,7 @@ CREATE TABLE `events` (
   `user_id` int(10) NOT NULL,
   `eventtopic` text NOT NULL,
   `eventname` text NOT NULL,
-  `eventtime` DATE NOT NULL, /*Only if the eventtype is Day(0) or sUntil(2)*/
+  `eventtime` DATE, /*Only if the eventtype is Day(0) or sUntil(2)*/
   `eventtype` int(1) NOT NULL, /*(0)Single day, deletes itself after completion. (1)Indefinite, requires user deletion. (2)Until, will delete after certain calendar day*/
   PRIMARY KEY (`event_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- eventadd.php
+++ eventadd.php
@@ -7,47 +7,70 @@
         // Check that they're logged in, otherwise redirect
         if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
         {
-          if(isset($_POST['eventname'])){
-            printf("Adding eventname");
-            $eventname = $_POST['eventname'];
+          if(isset($_POST['timeconstraint'])) {
+            $timeconstraint = $_POST['timeconstraint'];
+            
+            if($timeconstraint == 'oneday' || $timeconstraint == 'until'){
+              ?><h1>Select A Date</h1> <!--Jim-senpai pls add css and classes thx-->
+              <form action="eventadd.php" method="post" autocomplete="off">
+                  <input type="date" name="selecteddate">
+                 </form><?php
+            } else {
+              $selecteddate = null;
+            }
+            if(isset($_POST['eventname'])){
+              $eventname = $_POST['eventname'];
+            }
+            if(isset($_POST['selecteddate'])){
+              $selecteddate = $_POST['selecteddate'];
+            }
+            
+            if(isset($_POST['selectedtopic']) && isset($eventname) && $eventname != '' && isset($selecteddate) && $selecteddate != ''){
+              ?><h1>Event name <?php echo $eventname?></h1>
+              <?php
+              $eventname = $curLink->real_escape_string($_POST['eventname']);
+              $selectedtopic = $curLink->real_escape_string($_POST['selectedtopic']);
+              $selecteddate = $_POST['']
+              ?><h1>Parameters: eventname: <?php echo $eventname?> selectedtopic: <?php echo $selectedtopic?></h1>
+              <?php
+              $curLink->query("INSERT INTO events (user_id, eventtopic, eventname, eventtime, eventtype) VALUES(
+                  '".$_SESSION['id']."',
+                  '$selectedtopic',
+                  '$eventname',
+                  '$selecteddate',
+                  0)") or die(mysqli_error($curLink));
+              header('Refresh: 0');
+            }
+              ?>
+              <form action="eventadd.php" method="post" autocomplete="off">
+                <h1>Step Two: Name your Task:</h1></div>
+                <input type="text" name="eventname">
+                </div><br>
+                <h1>Step Three: Choose a Topic</h1>
+                <?php 
+              $temp = $curLink->query("SELECT * FROM `topics`");
+              $topics = $temp->fetch_all(MYSQLI_ASSOC);
+                foreach ($topics as $t) { ?>
+              <div>
+                
+                  <input type="submit" name="selectedtopic" value="<?= $t['topicname']?>">
+                </div>
+                <?php }
+            ?></form><?php
+            
+          }else{
+            ?><h1>Step One: Choose a Time:</h1><br>
+            <form action="eventadd.php" method="post" autocomplete="off">
+              <input type="radio" name="timeconstraint" value="oneday">
+              For One Day<br>
+              <input type="radio" name="timeconstraint" value="indefinite">
+              Until Turned Off<br>
+              <input type="radio" name="timeconstraint" value="until">
+              Everyday Until<br>
+              <input type="submit" value="Submit">
+            </form>
+          <?php }
           }
-          ?><h1>Event name <?php echo $eventname?></h1>
-            <?php
-          if(isset($_POST['selectedtopic']) && isset($eventname) && $eventname != ''){
-            ?><h1>Event name <?php echo $eventname?></h1>
-            <?php
-            $eventname = $_POST['eventname'];
-            $selectedtopic = $_POST['selectedtopic'];
-            
-            $curLink->query("INSERT INTO events (eventname, eventtime, eventtype) VALUES(
-              $eventname,
-              0,
-              0
-              ");
-          }
-          ?>
-          <form action="eventadd.php" method="post" autocomplete="off">
-            <h1>Step One: Name your Task:</h1></div>
-            <input type="text" name="eventname">
-            <h1>Step Two: Choose a Time:</h1><br>
-            <input type="button" name="onday" value="For One Day">
-            <input type="button" name="indefinite" value="Until Removed">
-            <input type="button" name="until" value="Everyday Until">
-            </div><br>
-            <h1>Step Three: Choose a Topic</h1>
-          <?php 
-          $temp = $curLink->query("SELECT * FROM `topics`");
-          $topics = $temp->fetch_all(MYSQLI_ASSOC);
-          foreach ($topics as $t) { ?>
-          <div> 
-          <!--Jim-senpai pls add css and classes thx-->
-            
-              <input type="submit" name="selectedtopic" value="<?= $t['topicname']?>">
-            
-          </div>
-        <?php }
-        ?></form><?php
-        }
         else
         {
           header("Location: login.php"); // Redirect to the login page

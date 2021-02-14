efb8e4a5aa2ade9abce1807629977a76dcc7f962
Ana Coda <anacod@zoho.com>
2021-02-13 19:42:03 -0700

    FINALLY

--- eventadd.php
+++ eventadd.php
@@ -7,26 +7,45 @@
         // Check that they're logged in, otherwise redirect
         if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
         {
-          if(isset($_POST['timeconstraint'])) {
+          if(isset($_POST['timeconstraint']) && (isset($_POST['first_submit']))) {
             $timeconstraint = $_POST['timeconstraint'];
-            
+          }
+          if(isset($timeconstraint) && (isset($_POST['first_submit']))){
+            printf($timeconstraint);
             if($timeconstraint == 'oneday' || $timeconstraint == 'until'){
               ?><h1>Select A Date</h1> <!--Jim-senpai pls add css and classes thx-->
-              <form action="eventadd.php" method="post" autocomplete="off">
+              <form action="" method="POST" autocomplete="off">
                   <input type="date" name="selecteddate">
                  <?php
             } else {
               ?>
-              <form action="eventadd.php" method="post" autocomplete="off"> <?php
+              <form action="" method="POST" autocomplete="off"> <?php
               $selecteddate = null;
             }
-            if(isset($_POST['eventname'])){
+            printf("after else");
+            if(isset($_POST['eventname']) && (isset($_POST['selectedtopic']))){
               $eventname = $_POST['eventname'];
+              printf("we have an eventname");
             }
+            printf($eventname && (isset($_POST['selectedtopic'])));
             if(isset($_POST['selecteddate'])){
               $selecteddate = $_POST['selecteddate'];
             }
-            if(isset($_POST['selectedtopic']) && isset($eventname) && $eventname != ''/* && isset($selecteddate) && $selecteddate != ''*/){
+          }
+          else{
+            ?><h1>Step One: Choose a Time:</h1><br>
+            <form action="eventadd.php" method="POST" autocomplete="off">
+              <input type="radio" name="timeconstraint" value="oneday">
+              For One Day<br>
+              <input type="radio" name="timeconstraint" value="indefinite">
+              Until Turned Off<br>
+              <input type="radio" name="timeconstraint" value="until">
+              Everyday Until<br>
+              <input type="submit" name="first_submit" value="Submit">
+            </form>
+          <?php }
+            printf($selecteddate);
+            if(isset($_POST['selectedtopic']) /*&& isset($eventname) && $eventname != '' && isset($selecteddate) && $selecteddate != ''*/){
               ?><h1>Event name <?php echo $eventname?></h1>
               <?php
               $eventname = $curLink->real_escape_string($_POST['eventname']);
@@ -49,24 +68,10 @@
               $temp = $curLink->query("SELECT * FROM `topics`");
               $topics = $temp->fetch_all(MYSQLI_ASSOC);
                 foreach ($topics as $t) { ?>
-              <div>
                   <input type="submit" name="selectedtopic" value="<?= $t['topicname']?>">
-                </div>
                 <?php }
             ?></form><?php
             
-          }else{
-            ?><h1>Step One: Choose a Time:</h1><br>
-            <form action="eventadd.php" method="post" autocomplete="off">
-              <input type="radio" name="timeconstraint" value="oneday">
-              For One Day<br>
-              <input type="radio" name="timeconstraint" value="indefinite">
-              Until Turned Off<br>
-              <input type="radio" name="timeconstraint" value="until">
-              Everyday Until<br>
-              <input type="submit" value="Submit">
-            </form>
-          <?php }
           }
         else
         {
